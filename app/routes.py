5d146afcfe618abd8ab3bd74d5f43b207081dc78
Daniil Boiko <daniilboiko@icloud.com>
2018-04-28 19:46:43 +0300

    Logo and adming page

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from app import app
 
 
@@ -12,3 +12,10 @@ def index():
 def search():
     query = request.args.get('query')
     return render_template('search.html', title='Home',query=query)
+
+@app.route('/admin', methods=['GET'])
+def admin():
+    token = request.args.get('token')
+    if token != '64E80F015881BF456198E9DAECB22B23D52CC45E2DE4708780E20F0E28F76CB0':
+        return redirect(url_for('index'))
+    return render_template('admin.html', title='admin')
