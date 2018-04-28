4417276213b8d3853828d4d41199bff5ef84599b
Daniil Boiko <daniilboiko@icloud.com>
2018-04-28 19:12:33 +0300

    Database connection

--- app/__init__.py
+++ app/__init__.py
@@ -10,4 +10,12 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
 
+from app import models
+
+db.init_app(app)
+with app.app_context():
+    # Extensions like Flask-SQLAlchemy now know what the "current" app
+    # is while within this block. Therefore, you can now run........
+    db.create_all()
+
 from app import routes, models
