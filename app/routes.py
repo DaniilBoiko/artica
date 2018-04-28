f9885c762859f340b0c2491a9369d59d8c12cfc6
TheSinSay9 <guschin1998@gmail.com>
2018-04-28 21:04:13 +0300

    parse xml

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,70 @@ from app import q, Job, conn
 
 def parseXMLs():
     result = []
+	def parseXML():
+    for i in range(1, 2):
+        with open('/data/ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed18n{0}.xml'.format(str('{:04d}'.format(i)))) as fobj:
+            doc = xmltodict.parse(fd.read())
+            for article in doc['PubmedArticleSet']['PubmedArticle']:
+                try:
+                    title = article['MedlineCitation']['Article']['ArticleTitle']
+                except KeyError:
+                    title = None  
+                ##########################################
+                try:
+                    abstract = article['MedlineCitation']['Article']['Abstract']['AbstractText']
+                except KeyError:
+                    abstract = None
+                ##########################################
+                try:
+                    year = int(article['MedlineCitation']['DateCompleted']['Year'])
+                    month = int(article['MedlineCitation']['DateCompleted']['Month'])
+                    day = int(article['MedlineCitation']['DateCompleted']['Day'])
+                    pubdate = datetime.date(year=year, month=month, day=day)
+                except KeyError:
+                    pubdate = None
+                ##########################################
+                try:
+                    volume = int(article['MedlineCitation']['Article']['Journal']['JournalIssue']['Volume'])
+                except KeyError:
+                    volume = None
+                ##########################################
+                try:
+                    issue = int(article['MedlineCitation']['Article']['Journal']['JournalIssue']['Issue'])
+                except KeyError:
+                    issue = None
+                ##########################################
+                try:
+                    journal = article['MedlineCitation']['Article']['Journal']['Title']
+                except KeyError:
+                    journal = None
+                ##########################################    
+                try:
+                    journalabbr = article['MedlineCitation']['Article']['Journal']['ISOAbbreviation']
+                except KeyError:
+                    journalabbr = None
+                ##########################################    
+                try:
+                    authors = article['MedlineCitation']['Article']['Journal']['Title']
+                except KeyError:
+                    authors = None
+                ##########################################
+                try:
+                    language = article['MedlineCitation']['Article']['Language']
+                except KeyError:
+                    language = None
+                ##########################################
+                try:
+                    keywords = []
+                    for topic in article['MedlineCitation']['MeshHeadingList']['MeshHeading']:
+                        keywords.append(topic['DescriptorName']['#text'])
+                except KeyError:
+                    keywords = None
+                ##########################################
+                try:
+                    issn = article['MedlineCitation']['Article']['Journal']['ISSN']['#text']
+                except KeyError:
+                    issn = None
     return result
 
 @app.route('/')
