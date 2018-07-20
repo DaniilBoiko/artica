e3b51ba4a41c7d74a27333cfebba7b1d1c76d962
Mikhail Konovalov <hjfarnsworth73@gmail.com>
2018-07-20 18:58:22 +0300

    bug

--- app/springer.py
+++ app/springer.py
@@ -175,6 +175,7 @@ def get_article(url):                   #счетчик
 
 
 def get_journal(url):
+    response = requests.get('https://link.springer.com/journal/volumesAndIssues/' + str(url))
     soup = BeautifulSoup(response.content, 'html.parser')
     title = soup.find('div', id='publication-title').find('h1').get_text()
     if Journal.query.filter_by(name = title).first() is None:
