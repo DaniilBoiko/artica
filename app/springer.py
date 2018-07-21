93c6e94065c41039f6b9afa25233485a119bffc6
Daniil Boiko <daniel.boyko.al@gmail.com>
2018-07-21 11:07:02 +0300

    try this one

--- app/springer.py
+++ app/springer.py
@@ -5,6 +5,7 @@ from app import db
 from multiprocessing import Pool
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from app import app
 
 user_agent = 'Googlebot'
 headers = {'User-Agent': user_agent}
@@ -177,43 +178,44 @@ def get_article(url):                   #счетчик
 
 
 def get_journal(url):
-    response = requests.get('https://link.springer.com/journal/volumesAndIssues/' + url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    title = soup.find('div', id='publication-title').find('h1').get_text()
-    print(title)
-    if Journal.query.filter_by(name = title).first() is None:
-        new_journal = Journal(name = title, link = 'https://link.springer.com/journal/' + url, publisher = 'Springer')
-        db.session.add(new_journal)
-        db.session.commit()
-
-    journal = Journal.query.filter_by(name = title).first()
-
-    issue_block = []
-    volume_tab = soup.find('div', class_='volumes tab-content')
-    for volume_item in volume_tab.find_all('div', class_='volume-item'):
-        issue_list = volume_item.find('ul', class_='issues-list')
-        for issue_item in issue_list.find_all('li', class_='issue-item'):
-            issue_block.append(issue_item.find('a', class_='title')['href'])
-
-    k = False
-    for issue_item in issue_block:
-        if journal.last_issue is not None:
-            if issue_item == journal.last_issue:
-                k = True
-
-        if (journal.last_issue is None) or k:
-            response = requests.get('https://link.springer.com' + issue_item)
-            soup = BeautifulSoup(response.content, 'html.parser')
-            results = soup.find('div', class_='toc')
-            for article_item in results.find_all('li'):
-                article_link = article_item.find('h3', class_='title').find('a')['href']
-                if Article.query.filter_by(doi = article_link[9:]).first() is None:
-                    get_article(article_link)
-
-            journal.last_issue = issue_item
+    with app.app_context():
+        response = requests.get('https://link.springer.com/journal/volumesAndIssues/' + url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.find('div', id='publication-title').find('h1').get_text()
+        print(title)
+        if Journal.query.filter_by(name = title).first() is None:
+            new_journal = Journal(name = title, link = 'https://link.springer.com/journal/' + url, publisher = 'Springer')
+            db.session.add(new_journal)
             db.session.commit()
 
-            k = True
+        journal = Journal.query.filter_by(name = title).first()
+
+        issue_block = []
+        volume_tab = soup.find('div', class_='volumes tab-content')
+        for volume_item in volume_tab.find_all('div', class_='volume-item'):
+            issue_list = volume_item.find('ul', class_='issues-list')
+            for issue_item in issue_list.find_all('li', class_='issue-item'):
+                issue_block.append(issue_item.find('a', class_='title')['href'])
+
+        k = False
+        for issue_item in issue_block:
+            if journal.last_issue is not None:
+                if issue_item == journal.last_issue:
+                    k = True
+
+            if (journal.last_issue is None) or k:
+                response = requests.get('https://link.springer.com' + issue_item)
+                soup = BeautifulSoup(response.content, 'html.parser')
+                results = soup.find('div', class_='toc')
+                for article_item in results.find_all('li'):
+                    article_link = article_item.find('h3', class_='title').find('a')['href']
+                    if Article.query.filter_by(doi = article_link[9:]).first() is None:
+                        get_article(article_link)
+
+                journal.last_issue = issue_item
+                db.session.commit()
+
+                k = True
 
 def get_springer(start,end):
     for item in range(int(start),int(end)):
