492889c58242887a6867c572292c959caf38fcd9
Daniil Boiko <daniel.boyko.al@gmail.com>
2018-07-28 22:34:25 +0300

    delete mendeley

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,6 @@ import xmltodict
 import time
 from bs4 import BeautifulSoup
 from flask import render_template, request, redirect, url_for, jsonify, make_response, session, current_app
-from mendeley import Mendeley
-from mendeley.session import MendeleySession
 from sqlalchemy import func, between
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.sql.expression import Executable, ClauseElement, _literal_as_text
@@ -39,13 +37,6 @@ def count_estimate(query, session, threshold=None):
     if threshold is not None and count < threshold:
         return query.count()
     return count
-
-
-def get_session_from_cookies():
-    return MendeleySession(mendeley, session['token'])
-
-
-mendeley = Mendeley('5691', 'Q87rg9xQ58L2HDav', 'http://ec2-18-220-156-220.us-east-2.compute.amazonaws.com:8080/oauth')
 
 
 class explain(Executable, ClauseElement):
@@ -386,177 +377,6 @@ def get_results_wiley(job_key):
                            start=job.meta['start'],
                            end=job.meta['end'], index=job.meta['index'], year=job.meta['year']), 202
 
-
-@app.route("/add_data", methods=['GET'])
-def add_data():
-    id = request.args.get('id')
-
-    if id is None:
-        return "ERROR", 202
-
-    doi = request.args.get('doi', '')
-    doctype = request.args.get('doctype', '')
-    crossref = request.args.get('crossref', '')
-    title = request.args.get('title', '')
-
-    article = Article.query.get_or_404(int(id))
-
-    if article.title is not None:
-        title1 = article.title[2:]
-        title1 = article.title[:-1]
-        if article.title[0] == '[':
-            title1 = article.title[1:]
-            title1 = str(article.title[:-2]) + '.'
-    else:
-        title1 = ' '
-
-    if distance(title1[0:-1].lower(), title.lower()) < 6:
-        article.doi = doi
-        article.doctype = doctype
-        article.crossref = crossref
-
-        db.session.commit()
-
-    return "OK", 200
-
-
-# Mendeley
-
-@app.route('/login')
-def login():
-    if 'token' in session:
-        return redirect('/listDocuments')
-
-    auth = mendeley.start_authorization_code_flow()
-    session['state'] = auth.state
-
-    return render_template('login.html', login_url=(auth.get_login_url()))
-
-
-@app.route('/oauth')
-def auth_return():
-    auth = mendeley.start_authorization_code_flow(state=session['state'])
-    mendeley_session = auth.authenticate(request.url)
-
-    session.clear()
-    session['token'] = mendeley_session.token
-
-    if User.query.filter_by(email=mendeley_session.profiles.me.email).first() is None:
-        new_user = User(mendeley_id=mendeley_session.profiles.me.id,
-                        first_name=mendeley_session.profiles.me.first_name,
-                        last_name=mendeley_session.profiles.me.last_name,
-                        display_name=mendeley_session.profiles.me.display_name,
-                        email=mendeley_session.profiles.me.email,
-                        created=datetime.datetime.today())
-        db.session.add(new_user)
-        db.session.commit()
-        if mendeley_session.documents.list(view='client').items is not None:
-            for document in mendeley_session.documents.list(view='client').items:
-
-                authors = []
-                if document.authors is not None:
-                    for author in document.authors:
-                        authors.append(author.first_name + ' ' + author.last_name)
-
-                user_doc = UserDocument(mendeley_id=document.id,
-                                        title=document.title,
-                                        type=document.type,
-                                        source=document.source,
-                                        year=document.year,
-                                        identifiers=document.identifiers,
-                                        keywords=document.keywords,
-                                        abstract=document.abstract,
-                                        authors=authors,
-                                        user=User.query.filter_by(email=mendeley_session.profiles.me.email).first().id)
-                db.session.add(user_doc)
-                db.session.commit()
-    return redirect('/listDocuments')
-
-
-@app.route('/listDocuments')
-def list_documents():
-    if 'token' not in session:
-        return redirect('/')
-
-    try:
-        mendeley_session = get_session_from_cookies()
-        name = mendeley_session.profiles.me.display_name
-    except:
-        return redirect('/logout')
-
-    docs = mendeley_session.documents.list(view='client').items
-
-    return render_template('library.html', name=name, docs=docs, title='Library')
-
-
-@app.route('/download')
-def download():
-    if 'token' not in session:
-        return redirect('/')
-
-    mendeley_session = get_session_from_cookies()
-
-    document_id = request.args.get('document_id')
-    doc = mendeley_session.documents.get(document_id)
-    doc_file = doc.files.list().items[0]
-
-    return redirect(doc_file.download_url)
-
-
-@app.route('/document')
-def get_document():
-    if 'token' not in session:
-        return redirect('/')
-
-    try:
-        mendeley_session = get_session_from_cookies()
-        name = mendeley_session.profiles.me.display_name
-    except:
-        return redirect('/logout')
-
-    document_id = request.args.get('document_id')
-    doc = mendeley_session.documents.get(document_id)
-
-    return render_template('metadata.html', doc=doc, name=name, title=doc['title'])
-
-
-@app.route('/metadataLookup')
-def metadata_lookup():
-    if 'token' not in session:
-        return redirect('/')
-
-    try:
-        mendeley_session = get_session_from_cookies()
-        name = mendeley_session.profiles.me.display_name
-    except:
-        return redirect('/logout')
-
-    doi = request.args.get('doi')
-    doc = mendeley_session.catalog.by_identifier(doi=doi)
-
-    return render_template('metadata.html', doc=doc, name=name, title=doc['title'])
-
-
-@app.route('/account')
-def account():
-    if 'token' not in session:
-        return redirect('/')
-
-    try:
-        mendeley_session = get_session_from_cookies()
-        name = mendeley_session.profiles.me.display_name
-    except:
-        return redirect('/logout')
-
-    email = mendeley_session.profiles.me.email
-
-    return render_template('account.html', name=name, email=email)
-
-
-@app.route('/logout')
-def logout():
-    session.pop('token', None)
-    return redirect('/')
 
 
 def parse_them_all():
